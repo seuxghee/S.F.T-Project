5b32e1779df9208313afa5098995c53eb553c829
SEUNGHEE <did030930@gmail.com>
2024-04-05 23:24:04 +0900

    Update MemberActivity.java

--- project/MemberActivity.java
+++ project/MemberActivity.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MemberActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member);
 
+        
+Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent); // button2 누르면 다시 LoginActivity로 돌아감.
+            }
+        });
 
 
 
