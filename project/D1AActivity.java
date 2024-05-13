1d7cf8e29ac17ec91d48d76b96f8d6397cb007f5
yangfaring <did030930@gmail.com>
2024-05-13 09:52:58 +0900

    Update D1AActivity.java

--- project/D1AActivity.java
+++ project/D1AActivity.java
@@ -23,7 +23,7 @@ public class D1AActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(getApplicationContext(),D1Activity.class);
-                startActivity(intent); // 뒤로 돌아가는 버튼
+                startActivity(intent); //D1Activity로 화면 전환
             }
         });
 
