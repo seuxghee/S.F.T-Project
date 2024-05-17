598643514181212b96d7d23eb9333c8b474b5b17
yangfaring <did030930@gmail.com>
2024-05-17 23:17:00 +0900

    Add files via upload

---
+++ project/D2AActivity.java
@@ -0,0 +1,29 @@
+package com.cookandroid.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class D2AActivity extends AppCompatActivity {
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d2_a);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),D2Activity.class);
+                startActivity(intent); // hoe
+            }
+        });
+    }
+}
