d52d5a14a0b20daba6ddae322c09fcfb9d619dea
yangfaring <did030930@gmail.com>
2024-05-21 20:22:43 +0900

    알레르기

---
+++ project/D3Activity.java
@@ -0,0 +1,70 @@
+package com.cookandroid.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class D3Activity extends AppCompatActivity {
+    Button button2;
+    ImageButton imgButton4, imageButton5, imageButton6, imagebutton2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d3);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        ImageButton imgButton4 = (ImageButton) findViewById(R.id.imgButton4);
+        ImageButton imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        ImageButton imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+        ImageButton imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DictionaryActivity.class);
+                startActivity(intent); // 딕셔너리 액티비티로 이동
+            }
+        });
+
+        imgButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), D3AActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), D3BActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), D3CActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),D1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
