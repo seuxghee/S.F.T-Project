f418ed58ff50fdcb9e7ca2ccf7f51aee12ae6782
yangfaring <did030930@gmail.com>
2024-05-24 16:22:05 +0900

    무좀 치료법

---
+++ project/D5CActivity.java
@@ -0,0 +1,30 @@
+package com.cookandroid.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class D5CActivity extends AppCompatActivity {
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d5_c);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),D5Activity.class);
+                startActivity(intent); // hoe
+            }
+        });
+    }
+}
